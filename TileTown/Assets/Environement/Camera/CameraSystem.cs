466c3c149a906b05702ee61abfebab1b0466bc8f
Codingwal <florian.herzog08@gmail.com>
2023-08-18 19:17:07 +0200

    More setup, begun camera system

---
+++ TileTown/Assets/Environement/Camera/CameraSystem.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSystem : MonoBehaviour
+{
+    private void Update()
+    {
+        Vector3 moveDir = new(0, 0, 0);
+
+        
+    }
+}
