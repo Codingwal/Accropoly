17123b3b70071e9265ff8d3acc90ba54fb39f528
Codingwal <florian.herzog08@gmail.com>
2024-09-01 19:51:35 +0200

    Remade TileSpawnerSystem
    Prepared for different meshes

--- Accropoly/Assets/0_Scripts/Systems/TileSpawnerSystem.cs
+++ Accropoly/Assets/0_Scripts/Systems/TileSpawnerSystem.cs
@@ -19,44 +19,30 @@ public partial struct TileSpawnerSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        TilePrefab prefab = SystemAPI.GetSingleton<TilePrefab>();
+        Entity prefabHolder = SystemAPI.GetSingletonEntity<TilePrefab>();
+        TilePrefab prefab = SystemAPI.GetComponent<TilePrefab>(prefabHolder);
+        state.EntityManager.RemoveComponent<TilePrefab>(prefabHolder);
+
         WorldData worldData = WorldDataManager.worldData;
-        var commandBufferSystem = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>();
+        EntityCommandBuffer commandBuffer = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(state.WorldUnmanaged);
 
-        InstantiateJob instantiateJob = new()
+        Tile[,] tiles = worldData.map.tiles;
+        for (int x = 0; x < tiles.GetLength(0); x++)
         {
-            commandBuffer = commandBufferSystem.CreateCommandBuffer(state.WorldUnmanaged),
-            tiles = worldData.map.tiles,
-            prefab = prefab,
-        };
-
-        state.Dependency = instantiateJob.Schedule(state.Dependency);
-    }
-    [BurstCompile]
-    private partial struct InstantiateJob : IJobEntity
-    {
-        public EntityCommandBuffer commandBuffer;
-        public Tile[,] tiles;
-        public TilePrefab prefab;
-        public void Execute()
-        {
-            for (int x = 0; x < tiles.GetLength(0); x++)
+            for (int y = 0; y < tiles.GetLength(1); y++)
             {
-                for (int y = 0; y < tiles.GetLength(1); y++)
+                Entity entity = commandBuffer.Instantiate(prefab);
+
+                commandBuffer.SetComponent(entity, LocalTransform.FromPosition(new(x, 1, y)));
+
+                foreach (var component in tiles[x, y].components)
                 {
-                    Entity entity = commandBuffer.Instantiate(prefab);
+                    Type type = component.GetType();
 
-                    commandBuffer.SetComponent(entity, LocalTransform.FromPosition(new(x, 1, y)));
+                    commandBuffer.AddComponent(entity, type);
 
-                    foreach (var component in tiles[x, y].components)
-                    {
-                        Type type = component.GetType();
-
-                        commandBuffer.AddComponent(entity, type);
-
-                        if (type == typeof(MapTileComponent)) commandBuffer.SetComponent(entity, (MapTileComponent)component);
-                        else Debug.LogError($"Unexpected type {type.Name}");
-                    }
+                    if (type == typeof(MapTileComponent)) commandBuffer.SetComponent(entity, (MapTileComponent)component);
+                    else Debug.LogError($"Unexpected type {type.Name}");
                 }
             }
         }
