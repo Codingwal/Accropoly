57afa5e1889f1d36b4089801ef459b10450b1a19
Codingwal <florian.herzog08@gmail.com>
2024-09-03 11:32:10 +0200

    New InputSYSTEM

---
+++ Accropoly/Assets/0_Scripts/Components/InputData.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public struct InputData : IComponentData
+{
+    public CameraInputData camera;
+    public Vector2 mouseMove;
+    public bool shift;
+}
+public struct CameraInputData
+{
+    public float2 move;
+    public bool sprint;
+    public float scroll;
+    public float rotate;
+    public bool look;
+}
+public struct UIInputData : IComponentData, IEnableableComponent
+{
+    public enum Action
+    {
+        Ctrl,
+        Clear,
+        Menu,
+        Escape,
+        Hotkey
+    }
+    public Action action;
+    public int hotkey;
+}
+public struct PlacementInputData : IComponentData, IEnableableComponent
+{
+    public enum Action
+    {
+        Rotate,
+        Place,
+        Cancel
+    }
+    public Action action;
+}
---
+++ Accropoly/Assets/0_Scripts/Systems/InputSystem.cs
@@ -0,0 +1,75 @@
+using Unity.Entities;
+using UnityEngine;
+
+using UIAction = UIInputData.Action;
+using PlacementAction = PlacementInputData.Action;
+
+[UpdateInGroup(typeof(InitializationSystemGroup))]
+public partial class InputSystem : SystemBase
+{
+    private Controls inputActions;
+    private Entity inputDataHolder;
+    protected override void OnCreate()
+    {
+        RequireForUpdate<RunGameTag>();
+
+        inputActions = new();
+        inputActions.Enable();
+
+        inputDataHolder = EntityManager.CreateEntity(typeof(InputData), typeof(UIInputData), typeof(PlacementInputData));
+
+        SystemAPI.SetComponentEnabled<UIInputData>(inputDataHolder, false);
+        SystemAPI.SetComponentEnabled<PlacementInputData>(inputDataHolder, false);
+
+        inputActions.InGame.Rotate.performed += (ctx) => OnPlacementAction(PlacementAction.Rotate);
+        inputActions.InGame.Place.performed += (ctx) => OnPlacementAction(PlacementAction.Place);
+        inputActions.InGame.Cancel.performed += (ctx) => OnPlacementAction(PlacementAction.Cancel);
+        inputActions.UI.Escape.performed += (ctx) => OnUIAction(UIAction.Escape); ;
+        inputActions.UI.Menu.performed += (ctx) => OnUIAction(UIAction.Menu); ;
+        inputActions.UI.Clear.performed += (ctx) => OnUIAction(UIAction.Clear); ;
+        inputActions.UI.Hotkey1.performed += (ctx) => OnUIAction(UIAction.Hotkey, 1); ;
+        inputActions.UI.Hotkey2.performed += (ctx) => OnUIAction(UIAction.Hotkey, 2); ;
+        inputActions.UI.Hotkey3.performed += (ctx) => OnUIAction(UIAction.Hotkey, 3); ;
+        inputActions.UI.Hotkey4.performed += (ctx) => OnUIAction(UIAction.Hotkey, 4); ;
+        inputActions.UI.Hotkey5.performed += (ctx) => OnUIAction(UIAction.Hotkey, 5); ;
+        inputActions.UI.Hotkey6.performed += (ctx) => OnUIAction(UIAction.Hotkey, 6); ;
+        inputActions.UI.Hotkey7.performed += (ctx) => OnUIAction(UIAction.Hotkey, 7); ;
+    }
+    protected override void OnUpdate()
+    {
+        // Update InputData
+        var inGameActions = inputActions.InGame;
+        SystemAPI.SetComponent(inputDataHolder, new InputData
+        {
+            camera = new CameraInputData
+            {
+                move = inGameActions.CameraMovement.ReadValue<Vector2>(),
+                sprint = inGameActions.CameraSprint.IsPressed(),
+                scroll = inGameActions.CameraScroll.ReadValue<float>(),
+                rotate = inGameActions.CameraRotation.ReadValue<float>(),
+                look = inGameActions.CameraLook.IsPressed(),
+            },
+            mouseMove = inGameActions.MouseMove.ReadValue<Vector2>(),
+            shift = inGameActions.Shift.IsPressed(),
+        });
+
+        // Disable input event tags
+        SystemAPI.SetComponentEnabled<PlacementInputData>(inputDataHolder, false);
+        SystemAPI.SetComponentEnabled<UIInputData>(inputDataHolder, false);
+    }
+    private void OnPlacementAction(PlacementAction action)
+    {
+        SystemAPI.SetComponentEnabled<PlacementInputData>(inputDataHolder, true);
+        SystemAPI.SetComponent(inputDataHolder, new PlacementInputData { action = action });
+    }
+    private void OnUIAction(UIAction action, int hotkey = -1)
+    {
+        SystemAPI.SetComponentEnabled<UIInputData>(inputDataHolder, true);
+        SystemAPI.SetComponent(inputDataHolder, new UIInputData
+        {
+            action = action,
+            hotkey = hotkey
+        });
+    }
+
+}
