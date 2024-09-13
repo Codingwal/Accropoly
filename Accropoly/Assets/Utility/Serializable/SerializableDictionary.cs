b450878f5818dff483ee777841fa922b8acdf1c5
Codingwal <florian.herzog08@gmail.com>
2024-09-13 18:27:19 +0200

    Improved SerializableDictionary

--- Accropoly/Assets/Utility/Serializable/SerializableDictionary.cs
+++ Accropoly/Assets/Utility/Serializable/SerializableDictionary.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
-public class SerializableDictionary<TKey, TValue>
+public class SerializableDictionary<TKey, TValue> : IEnumerable<SerializableKeyValuePair<TKey, TValue>>
 {
     [SerializeField] private List<SerializableKeyValuePair<TKey, TValue>> dictionary = new();
 
@@ -34,6 +35,47 @@ public class SerializableDictionary<TKey, TValue>
             }
         }
         return false;
+    }
+
+    public IEnumerator<SerializableKeyValuePair<TKey, TValue>> GetEnumerator()
+    {
+        return (IEnumerator<SerializableKeyValuePair<TKey, TValue>>)new Enumerator(dictionary);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public static implicit operator Dictionary<TKey, TValue>(SerializableDictionary<TKey, TValue> dict)
+    {
+        Dictionary<TKey, TValue> newDict = new();
+        foreach (var pair in dict)
+        {
+            if (!newDict.TryAdd(pair.key, pair.value))
+                Debug.LogError($"Duplicate key {pair.key} in SerializableDictionary<{typeof(TKey)}, {typeof(TValue)}>");
+        }
+        return newDict;
+    }
+    public class Enumerator : IEnumerator
+    {
+        private List<SerializableKeyValuePair<TKey, TValue>> dict;
+        private int index = -1;
+        public object Current => dict[index];
+        public Enumerator(List<SerializableKeyValuePair<TKey, TValue>> dict)
+        {
+            this.dict = dict;
+        }
+        public bool MoveNext()
+        {
+            index++;
+            return index < dict.Count;
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
     }
 }
 
