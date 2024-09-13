d210111ae0d1acd4b8ecc1fab8e851b4c543616b
Codingwal <florian.herzog08@gmail.com>
2024-09-13 11:45:49 +0200

    Created TileGridUtility file

--- Accropoly/Assets/0_Scripts/Components/MapTileComponent.cs
+++ Accropoly/Assets/0_Scripts/Components/MapTileComponent.cs
@@ -9,32 +9,6 @@ public struct MapTileComponent : IComponentData
         pos = new(x, y);
         this.tileType = tileType;
     }
-    public static DynamicBuffer<EntityBufferElement> GetEntityGrid()
-    {
-        var em = World.DefaultGameObjectInjectionWorld.EntityManager;
-        var query = em.CreateEntityQuery(typeof(EntityGridHolder));
-        return em.GetBuffer<EntityBufferElement>(query.GetSingletonEntity());
-    }
-    public static int GetIndex(int2 pos, int totalMapSize)
-    {
-        return pos.x * (int)math.sqrt(totalMapSize) + pos.y;
-    }
-    public static Entity GetTile(int2 pos)
-    {
-        var buffer = GetEntityGrid();
-        return buffer[GetIndex(pos, buffer.Length)];
-    }
-    public static Entity[] GetNeighbourTiles(int2 pos)
-    {
-        var buffer = GetEntityGrid();
-        return new Entity[4]
-        {
-            buffer[GetIndex(pos + new int2(1, 0), buffer.Length)],
-            buffer[GetIndex(pos + new int2(-1, 0), buffer.Length)],
-            buffer[GetIndex(pos + new int2(0, 1), buffer.Length)],
-            buffer[GetIndex(pos + new int2(0, -1), buffer.Length)],
-        };
-    }
 }
 public enum TileType
 {
---
+++ Accropoly/Assets/Utility/Map/TileGridUtility.cs
@@ -0,0 +1,32 @@
+using Unity.Entities;
+using Unity.Mathematics;
+
+public static class TileGridUtility
+{
+    public static DynamicBuffer<EntityBufferElement> GetEntityGrid()
+    {
+        var em = World.DefaultGameObjectInjectionWorld.EntityManager;
+        var query = em.CreateEntityQuery(typeof(EntityGridHolder));
+        return em.GetBuffer<EntityBufferElement>(query.GetSingletonEntity());
+    }
+    public static int GetIndex(int2 pos, int totalMapSize)
+    {
+        return pos.x * (int)math.sqrt(totalMapSize) + pos.y;
+    }
+    public static Entity GetTile(int2 pos)
+    {
+        var buffer = GetEntityGrid();
+        return buffer[GetIndex(pos, buffer.Length)];
+    }
+    public static Entity[] GetNeighbourTiles(int2 pos)
+    {
+        var buffer = GetEntityGrid();
+        return new Entity[4]
+        {
+            buffer[GetIndex(pos + new int2(1, 0), buffer.Length)],
+            buffer[GetIndex(pos + new int2(-1, 0), buffer.Length)],
+            buffer[GetIndex(pos + new int2(0, 1), buffer.Length)],
+            buffer[GetIndex(pos + new int2(0, -1), buffer.Length)],
+        };
+    }
+}
