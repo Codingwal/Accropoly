7bbd72e8253e6d10cf6356f4966c8134e7d2f42c
Codingwal <florian.herzog08@gmail.com>
2024-09-14 21:38:20 +0200

    Prepared ElectricitySystem

---
+++ Accropoly/Assets/0_Scripts/ECS/Systems/TileMapSimulation/ElectricitySystem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public partial struct ElectricitySystem : ISystem
+{
+    public void OnCreate(ref SystemState state)
+    {
+
+    }
+    public void OnUpdate(ref SystemState state)
+    {
+
+    }
+}
