4da22acb622ad18ada9dc32907014394ab7a9a90
Codingwal <florian.herzog08@gmail.com>
2024-11-06 12:10:35 +0100

    refactor: delete unused properties
    Delete unused properties (previousProduction / previousConsumption) from electricity components

--- Accropoly/Assets/0_Scripts/ECS/Components/Tile/Electricity.cs
+++ Accropoly/Assets/0_Scripts/ECS/Components/Tile/Electricity.cs
@@ -5,23 +5,11 @@ using UnityEngine;
 
 public struct ElectricityProducer : IComponentData
 {
-    public float previousProduction;
     public float production;
-    public void UpdateProduction(float newProduction)
-    {
-        previousProduction = production;
-        production = newProduction;
-    }
 }
 public struct ElectricityConsumer : IComponentData
 {
-    public float previousConsumption;
     public float consumption;
-    public void UpdateConsumption(float newConsumption)
-    {
-        previousConsumption = consumption;
-        consumption = newConsumption;
-    }
 }
 public struct UpdatedElectricityTag : IComponentData { }
 public struct HasElectricityTag : IComponentData, IEnableableComponent { }
--- Accropoly/Assets/0_Scripts/ECS/Systems/TileMapSimulation/ElectricitySystem.cs
+++ Accropoly/Assets/0_Scripts/ECS/Systems/TileMapSimulation/ElectricitySystem.cs
@@ -30,50 +30,9 @@ public partial class ElectricitySystem : SystemBase
             totalConsumption += canEnable ? consumer.consumption : 0; // Only add to the production if the consumer can be enabled
             EntityManager.SetComponentEnabled<HasElectricityTag>(entity, canEnable);
 
-            maxConsumption += consumer.consumption; // Only for imformative purposes
+            maxConsumption += consumer.consumption; // Only for informative purposes
         }).WithoutBurst().Run();
 
-        // Debug.Log($"{maxConsumption}/{totalProduction}");
+        Debug.Log($"{maxConsumption}/{totalProduction}");
     }
 }
-
-// More efficient but also more complex electricty system. Disabled tiles are not handled correctly.
-/*
-Debug.Log($"{totalConsumption}/{totalProduction}");
-Entities.WithAll<ActiveTileTag>().WithAny<NewTileTag, UpdatedElectricityTag>().ForEach((in ElectricityProducer producer) =>
-{
-    totalProduction += producer.production - producer.previousProduction;
-}).WithoutBurst().Run();
-
-Entities.WithAll<ActiveTileTag>().WithAny<NewTileTag, UpdatedElectricityTag>().ForEach((in ElectricityConsumer consumer) =>
-{
-    totalConsumption += consumer.consumption - consumer.previousConsumption;
-}).WithoutBurst().Run();
-
-// Update enabled state of consumers
-if (totalConsumption > totalProduction) // If there is a lack of electricity...
-{
-    // Disable consumers until there no longer is a lack of electricity
-    bool enoughElectricity = false;
-    Entities.WithAll<ActiveTileTag>().ForEach((Entity entity, in ElectricityConsumer consumer) =>
-    {
-        if (enoughElectricity) return; // If there isn't a lack of electricity anymore, there's nothing more to do
-
-        SystemAPI.SetComponentEnabled<HasElectricityTag>(entity, false); // Disable the tile
-        totalConsumption -= consumer.consumption;
-
-        if (totalConsumption <= totalProduction) enoughElectricity = true;
-    }).WithoutBurst().Run();
-}
-else if (totalConsumption < totalProduction) // If there is more electricity than required...
-{
-    Entities.WithNone<HasElectricityTag>().ForEach((Entity entity, in ElectricityConsumer consumer) =>
-    {
-        if (totalConsumption + consumer.consumption < totalProduction) // If it can be enabled without creating a lack of electricity
-        {
-            totalConsumption += consumer.consumption;
-            SystemAPI.SetComponentEnabled<HasElectricityTag>(entity, true); // Reenable the tile
-        }
-    }).WithoutBurst().Run();
-}
-*/
