0ff56701d2dab00ab80b30b8dd05efc566098976
Codingwal <florian.herzog08@gmail.com>
2024-11-06 12:32:15 +0100

    Revert "test: make solar panels polluting"
    This reverts commit ee55a2f6de292dcea94d444234a10aca4adbd160.

--- Accropoly/Assets/0_Scripts/ECS/Systems/TileMapSimulation/ElectricityRenewabilitySystem.cs
+++ Accropoly/Assets/0_Scripts/ECS/Systems/TileMapSimulation/ElectricityRenewabilitySystem.cs
@@ -3,18 +3,8 @@ using UnityEngine;
 
 public partial class ElectricityRenewabilitySystem : SystemBase
 {
-    private uint frame;
-    protected override void OnCreate()
-    {
-        RequireForUpdate<Polluter>();
-        RequireForUpdate<ElectricityProducer>();
-    }
     protected override void OnUpdate()
     {
-        // Only run this function every 50 frames
-        frame++;
-        if (frame % 50 != 2) return;
-
         float totalPollution = 0;
         Entities.WithAll<ActiveTileTag, ElectricityProducer>().ForEach((in Polluter polluter) =>
         {
