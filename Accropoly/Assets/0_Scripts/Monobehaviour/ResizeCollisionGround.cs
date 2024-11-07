772b400477f125082f76b987e7f5b75fa6578ee9
Codingwal <florian.herzog08@gmail.com>
2024-11-07 15:11:42 +0100

    fix: resize collisionGround with map

---
+++ Accropoly/Assets/0_Scripts/Monobehaviour/ResizeCollisionGround.cs
@@ -0,0 +1,24 @@
+using Unity.Entities;
+using UnityEngine;
+
+public class ResizecollisionGround : MonoBehaviour
+{
+    EntityQuery query;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EntityManager em = World.DefaultGameObjectInjectionWorld.EntityManager;
+        query = em.CreateEntityQuery(typeof(LoadGameTag));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (query.CalculateEntityCount() != 0)
+        {
+            float mapScale = WorldDataSystem.worldData.map.tiles.GetLength(0);
+            transform.position = new(mapScale - 1, -0.01f, mapScale - 1);
+            transform.localScale = new(mapScale / 5, 1, mapScale / 5);
+        }
+    }
+}
