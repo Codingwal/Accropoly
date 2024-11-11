27513c30c97a589d67d3b821cb6986f1b022b58f
Codingwal <florian.herzog08@gmail.com>
2024-11-11 14:05:04 +0100

    fix: fix fatal bug
    GetEntityGrid() can't be used from jobs as it uses the EntityManager!

--- Accropoly/Assets/0_Scripts/Utility/Map/TileGridUtility.cs
+++ Accropoly/Assets/0_Scripts/Utility/Map/TileGridUtility.cs
@@ -20,15 +20,34 @@ public static class TileGridUtility
     {
         return pos.x * (int)math.sqrt(totalMapSize) + pos.y;
     }
+    /// <remarks>Can't be used in jobs!</remarks>
     public static Entity GetTile(int2 pos)
     {
         Entity entity = TryGetTile(pos, out bool entityExists);
         if (!entityExists) throw new($"Invalid position {pos}");
         return entity;
     }
+    /// <remarks>Can't be used in jobs!</remarks>
     public static Entity TryGetTile(int2 pos, out bool entityExists)
     {
         var buffer = GetEntityGrid();
+        int index = GetIndex(pos, buffer.Length);
+        if (index >= buffer.Length || index < 0)
+        {
+            entityExists = false;
+            return new();
+        }
+        entityExists = true;
+        return buffer[index];
+    }
+    public static Entity GetTile(int2 pos, DynamicBuffer<EntityBufferElement> buffer)
+    {
+        Entity entity = TryGetTile(pos, buffer, out bool entityExists);
+        if (!entityExists) throw new($"Invalid position {pos}");
+        return entity;
+    }
+    public static Entity TryGetTile(int2 pos, DynamicBuffer<EntityBufferElement> buffer, out bool entityExists)
+    {
         int index = GetIndex(pos, buffer.Length);
         if (index >= buffer.Length || index < 0)
         {
