d105f151faec72830839e4018418946f3f71a13f
Codingwal <florian.herzog08@gmail.com>
2024-11-13 13:59:22 +0100

    refactor: adapted MaterialsAndMeshesHolder

--- Accropoly/Assets/0_Scripts/ECS/Components/Tile/ConnectingTile.cs
+++ Accropoly/Assets/0_Scripts/ECS/Components/Tile/ConnectingTile.cs
@@ -15,6 +15,40 @@ public unsafe struct ConnectingTile : IComponentData
     {
         return connectableSides[(uint)direction.Rotate(-(int)(uint)rotation)];
     }
+    /// <summary>
+    ///  This doesn't contain the full component information! This is only used to select a MeshMaterialPair
+    /// </summary>
+    public int GetIndex()
+    {
+        int firstTrueIndex = NextTrueIndex(0);
+        if (firstTrueIndex == -1) return 0; // No connections
+
+        int secondTrueIndex = NextTrueIndex(firstTrueIndex + 1);
+        if (secondTrueIndex == -1) return 1; // 1 connection
+
+        int thirdTrueIndex = NextTrueIndex(secondTrueIndex + 1);
+        if (thirdTrueIndex == -1) // 2 connections
+            return (secondTrueIndex % 2 == firstTrueIndex) ? 2 : 3; // Check if they're adjacent (3 == adjacent)
+
+        int fourthTrueIndex = NextTrueIndex(thirdTrueIndex + 1);
+        if (fourthTrueIndex == -1) return 4; // 3 connections
+        return 5; // 4 connections
+    }
+    public Direction GetRotation()
+    {
+        int index = NextTrueIndex(0);
+        if (index == -1) return Directions.North;
+        else return (Direction)(uint)index;
+    }
+    private int NextTrueIndex(int start)
+    {
+        for (int i = start; i < 4; i++)
+            if (connectableSides[i])
+            {
+                return i;
+            }
+        return -1;
+    }
     public int Serialize()
     {
         Debug.Assert(sizeof(bool) * 4 == sizeof(int)); // Assert that bool[4] and int are of the same size
