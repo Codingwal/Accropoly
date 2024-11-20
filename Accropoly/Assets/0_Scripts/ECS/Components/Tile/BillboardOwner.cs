156dd1edb058118ba6dd8a7aabdaa7cf151619bd
Codingwal <florian.herzog08@gmail.com>
2024-11-20 16:30:18 +0100

    refactor: prepare for multiple problems

--- Accropoly/Assets/0_Scripts/ECS/Components/Tile/BillboardOwner.cs
+++ Accropoly/Assets/0_Scripts/ECS/Components/Tile/BillboardOwner.cs
@@ -1,9 +1,31 @@
+using Unity.Collections;
 using Unity.Entities;
 
 namespace Components
 {
     public struct BillboardOwner : IComponentData
     {
-        public Entity billboardEntity;
+        public NativeList<BillboardInfo> billboards;
+        public BillboardOwner(BillboardInfo element)
+        {
+            billboards = new(Allocator.Persistent)
+            {
+                element
+            };
+        }
+        public struct BillboardInfo
+        {
+            public Entity entity;
+            public Problems problem;
+            public BillboardInfo(Entity entity, Problems problem)
+            {
+                this.entity = entity;
+                this.problem = problem;
+            }
+            public enum Problems
+            {
+                NoElectricity,
+            }
+        }
     }
 }
--- Accropoly/Assets/0_Scripts/ECS/Systems/BillboardSystem.cs
+++ Accropoly/Assets/0_Scripts/ECS/Systems/BillboardSystem.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 using ConfigComponents;
 using Unity.Transforms;
 using Components;
+using Unity.Collections;
+
+using BillboardProblems = Components.BillboardOwner.BillboardInfo.Problems;
+using System.Linq;
 
 public partial class BillboardSystem : SystemBase
 {
@@ -26,33 +30,68 @@ public partial class BillboardSystem : SystemBase
         // Alternative to RequireForUpdate<RunGame>()
         if (!SystemAPI.HasSingleton<RunGame>()) return;
 
-        // Delete billboard if tile gets replaced
+        // Delete billboards if tile gets replaced
         Entities.WithAll<Replace>().ForEach((Entity entity, in BillboardOwner billboardOwner) =>
         {
-            ecb.DestroyEntity(billboardOwner.billboardEntity);
+            // Destroy all billboards
+            foreach (var info in billboardOwner.billboards)
+                ecb.DestroyEntity(info.entity);
 
-            ecb.RemoveComponent<BillboardOwner>(entity);
+            // Dispose the NativeList
+            billboardOwner.billboards.Dispose();
         }).Schedule();
 
         // Delete billboard if problem is fixed
-        Entities.WithAll<HasElectricity>().ForEach((Entity entity, in BillboardOwner billboardOwner) =>
+        Entities.WithAll<HasElectricity>().ForEach((Entity entity, ref BillboardOwner billboardOwner) =>
         {
-            ecb.DestroyEntity(billboardOwner.billboardEntity);
+            RemoveBillboard(ref billboardOwner.billboards, BillboardProblems.NoElectricity, ecb);
 
-            ecb.RemoveComponent<BillboardOwner>(entity);
+            // Check if there are no billboards left
+            if (billboardOwner.billboards.Length == 0)
+                ecb.RemoveComponent<BillboardOwner>(entity);
         }).Schedule();
 
         // Add billboard if there is a problem
         Entity prefab = SystemAPI.GetSingleton<BillboardPrefab>();
         Entities.WithNone<BillboardOwner>().WithDisabled<HasElectricity>().ForEach((Entity tileEntity, in LocalTransform transform) =>
         {
-            Entity entity = ecb.Instantiate(prefab);
-            LocalTransform transformCopy = transform;
-            transformCopy.Position.y = 1;
-            ecb.SetComponent(entity, transformCopy);
-            ecb.AddComponent<Billboard>(entity);
-
-            ecb.AddComponent(tileEntity, new BillboardOwner { billboardEntity = entity });
+            Entity newBillboard = AddBillboard(transform, prefab, ecb);
+            ecb.AddComponent(tileEntity, new BillboardOwner(new(newBillboard, BillboardProblems.NoElectricity)));
         }).Schedule();
+        Entities.WithDisabled<HasElectricity>().ForEach((Entity tileEntity, ref BillboardOwner billboardOwner, in LocalTransform transform) =>
+        {
+            if (ContainsProblem(billboardOwner.billboards, BillboardProblems.NoElectricity)) return; // Return if this problem has already been added
+
+            // Create billboard and add it to the billboardOwner list
+            Entity newBillboard = AddBillboard(transform, prefab, ecb);
+            billboardOwner.billboards.Add(new(newBillboard, BillboardProblems.NoElectricity));
+        }).Schedule();
+    }
+    private static Entity AddBillboard(LocalTransform ownerTransform, Entity billboardPrefab, EntityCommandBuffer ecb)
+    {
+        Entity entity = ecb.Instantiate(billboardPrefab); // Create the billboard
+        ownerTransform.Position.y = 1; // Use owner transform but position in the air over it
+        ecb.SetComponent(entity, ownerTransform);
+        ecb.AddComponent<Billboard>(entity); // Tag component used by queries
+        return entity;
+    }
+    private static bool ContainsProblem(in NativeList<BillboardOwner.BillboardInfo> billboards, BillboardProblems problem)
+    {
+        for (int i = 0; i < billboards.Length; i++)
+            if (billboards[i].problem == problem)
+                return true;
+        return false;
+    }
+    private static void RemoveBillboard(ref NativeList<BillboardOwner.BillboardInfo> billboards, BillboardProblems problem, EntityCommandBuffer ecb)
+    {
+        for (int i = 0; i < billboards.Length; i++)
+        {
+            var info = billboards[i];
+            if (info.problem == problem)
+            {
+                ecb.DestroyEntity(info.entity);
+                billboards.RemoveAt(i);
+            }
+        }
     }
 }
