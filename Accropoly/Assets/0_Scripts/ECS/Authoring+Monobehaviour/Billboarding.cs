87d0326e66c9f0042e31fe0fe0ceba46dc561831
Codingwal <florian.herzog08@gmail.com>
2024-11-21 07:20:34 +0100

    refactor: prepare for multiple materials

--- Accropoly/Assets/0_Scripts/ECS/Authoring+Monobehaviour/Billboarding.cs
+++ Accropoly/Assets/0_Scripts/ECS/Authoring+Monobehaviour/Billboarding.cs
@@ -1,12 +1,19 @@
+using System;
+using System.Linq;
+using Components;
+using Unity.Collections;
 using Unity.Entities;
+using Unity.Rendering;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace Authoring
 {
     public class Billboarding : MonoBehaviour
     {
         [SerializeField] private GameObject prefab;
-
+        [SerializeField] private Mesh mesh;
+        [SerializeField] private SerializableDictionary<BillboardInfo.Problems, Material> materials;
         public class Baker : Baker<Billboarding>
         {
             public override void Bake(Billboarding authoring)
@@ -15,23 +22,33 @@ namespace Authoring
 
                 Entity entity = GetEntity(TransformUsageFlags.None);
 
-                AddComponent<ConfigComponents.BillboardPrefab>(entity, GetEntity(authoring.prefab, TransformUsageFlags.Dynamic));
+                var entitiesGraphicsSystem = World.DefaultGameObjectInjectionWorld.GetOrCreateSystemManaged<EntitiesGraphicsSystem>();
+
+                BatchMeshID meshID = entitiesGraphicsSystem.RegisterMesh(authoring.mesh);
+
+                int largestEnumValue = Enum.GetValues(typeof(BillboardInfo.Problems)).Cast<int>().Max();
+                NativeArray<BatchMaterialID> materialIDs = new(largestEnumValue, Allocator.Persistent);
+                foreach (var pair in authoring.materials)
+                {
+                    materialIDs[(int)pair.key] = entitiesGraphicsSystem.RegisterMaterial(pair.value);
+                }
+
+                AddComponent(entity, new ConfigComponents.Billboarding
+                {
+                    prefab = GetEntity(authoring.prefab, TransformUsageFlags.Dynamic),
+                    meshID = meshID,
+                    materialIDs = materialIDs,
+                });
             }
         }
     }
 }
 namespace ConfigComponents
 {
-    public struct BillboardPrefab : IComponentData
+    public struct Billboarding : IComponentData
     {
         public Entity prefab;
-        public static implicit operator BillboardPrefab(Entity entity)
-        {
-            return new BillboardPrefab { prefab = entity };
-        }
-        public static implicit operator Entity(BillboardPrefab config)
-        {
-            return config.prefab;
-        }
+        public BatchMeshID meshID;
+        public NativeArray<BatchMaterialID> materialIDs;
     }
 }
--- Accropoly/Assets/0_Scripts/ECS/Systems/BillboardSystem/CreateBillboards.cs
+++ Accropoly/Assets/0_Scripts/ECS/Systems/BillboardSystem/CreateBillboards.cs
@@ -6,6 +6,7 @@ using Components;
 using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 using Unity.Collections;
+using Unity.Rendering;
 
 
 namespace Systems
@@ -22,57 +23,64 @@ namespace Systems
         protected override void OnUpdate()
         {
             var ecb = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(World.Unmanaged);
-            Entity prefab = SystemAPI.GetSingleton<BillboardPrefab>();
+            var config = SystemAPI.GetSingleton<Billboarding>();
             NativeList<Entity> alreadyVisitedEntities = new(Allocator.TempJob);
 
             // Add billboard if there is a problem and the tile is not a billboard owner
             Entities.WithNone<BillboardOwner>().WithDisabled<HasElectricity>().ForEach((Entity tileEntity, in LocalTransform transform) =>
             {
-                AddBillboardNoBillboardOwner(tileEntity, transform, NoElectricityProblem, prefab, ecb, alreadyVisitedEntities);
+                AddBillboardNoBillboardOwner(tileEntity, transform, NoElectricityProblem, config, ecb, alreadyVisitedEntities);
             }).Schedule();
             Entities.WithNone<BillboardOwner>().WithDisabled<IsConnected>().ForEach((Entity tileEntity, in LocalTransform transform) =>
             {
-                AddBillboardNoBillboardOwner(tileEntity, transform, NotConnectedProblem, prefab, ecb, alreadyVisitedEntities);
+                AddBillboardNoBillboardOwner(tileEntity, transform, NotConnectedProblem, config, ecb, alreadyVisitedEntities);
             }).Schedule();
 
             // Add billboard if there is a problem and the tile is a billboard owner
             Entities.WithDisabled<HasElectricity>().ForEach((Entity tileEntity, ref BillboardOwner billboardOwner, in LocalTransform transform) =>
             {
-                AddBillboardToBillboardOwner(tileEntity, ref billboardOwner, transform, NoElectricityProblem, prefab, ecb, alreadyVisitedEntities);
+                AddBillboardToBillboardOwner(tileEntity, ref billboardOwner, transform, NoElectricityProblem, config, ecb, alreadyVisitedEntities);
             }).Schedule();
             Entities.WithDisabled<IsConnected>().ForEach((Entity tileEntity, ref BillboardOwner billboardOwner, in LocalTransform transform) =>
             {
-                AddBillboardToBillboardOwner(tileEntity, ref billboardOwner, transform, NotConnectedProblem, prefab, ecb, alreadyVisitedEntities);
+                AddBillboardToBillboardOwner(tileEntity, ref billboardOwner, transform, NotConnectedProblem, config, ecb, alreadyVisitedEntities);
             }).Schedule();
 
             alreadyVisitedEntities.Dispose(Dependency);
         }
-        private static void AddBillboardNoBillboardOwner(Entity tileEntity, in LocalTransform transform, BillboardInfo.Problems problem, Entity prefab, EntityCommandBuffer ecb, NativeList<Entity> alreadyVisitedEntities)
+        private static void AddBillboardNoBillboardOwner(Entity tileEntity, in LocalTransform transform, BillboardInfo.Problems problem, Billboarding config, EntityCommandBuffer ecb, NativeList<Entity> alreadyVisitedEntities)
         {
             if (alreadyVisitedEntities.Contains(tileEntity)) return;
 
-            Entity newBillboard = AddBillboard(transform, (int)problem + 1, prefab, ecb);
+            Entity newBillboard = AddBillboard(transform, problem, config, ecb);
             ecb.AddComponent(tileEntity, new BillboardInfo(newBillboard, problem)); // This will get added to the billboards list
             ecb.AddComponent(tileEntity, BillboardOwner.CreateInstance());
 
             alreadyVisitedEntities.Add(tileEntity);
         }
-        private static void AddBillboardToBillboardOwner(Entity tileEntity, ref BillboardOwner billboardOwner, in LocalTransform transform, BillboardInfo.Problems problem, Entity prefab, EntityCommandBuffer ecb, NativeList<Entity> alreadyVisitedEntities)
+        private static void AddBillboardToBillboardOwner(Entity tileEntity, ref BillboardOwner billboardOwner, in LocalTransform transform, BillboardInfo.Problems problem, Billboarding config, EntityCommandBuffer ecb, NativeList<Entity> alreadyVisitedEntities)
         {
             if (alreadyVisitedEntities.Contains(tileEntity)) return;
             if (ContainsProblem(billboardOwner.billboards, problem)) return; // Return if this problem has already been added
 
-            Entity newBillboard = AddBillboard(transform, (int)problem + 1, prefab, ecb);
+            Entity newBillboard = AddBillboard(transform, problem, config, ecb);
             ecb.AddComponent(tileEntity, new BillboardInfo(newBillboard, problem)); // This will get added to the billboards list
 
             alreadyVisitedEntities.Add(tileEntity);
         }
-        private static Entity AddBillboard(LocalTransform ownerTransform, float height, Entity billboardPrefab, EntityCommandBuffer ecb)
+        private static Entity AddBillboard(LocalTransform ownerTransform, BillboardInfo.Problems problem, Billboarding config, EntityCommandBuffer ecb)
         {
-            Entity entity = ecb.Instantiate(billboardPrefab); // Create the billboard
-            ownerTransform.Position.y = height; // Use owner transform but position in the air over it
+            // Create the billboard using the prefab
+            Entity entity = ecb.Instantiate(config.prefab);
+
+            // Use owner transform but position in the air over it
+            ownerTransform.Position.y = (int)problem + 1;
             ecb.SetComponent(entity, ownerTransform);
             ecb.AddComponent<Billboard>(entity); // Tag component used by queries
+
+            // Set material and mesh according to problem
+            ecb.SetComponent(entity, new MaterialMeshInfo(config.materialIDs[(int)problem], config.meshID));
+
             return entity;
         }
         private static bool ContainsProblem(in UnsafeList<BillboardInfo> billboards, BillboardInfo.Problems problem)
