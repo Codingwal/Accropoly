8736da16948cd85aa2b7fffb28a2040e45d9d38b
Codingwal <florian.herzog08@gmail.com>
2024-12-01 17:31:50 +0100

    feature: daily schedule

--- Accropoly/Assets/0_Scripts/ECS/Systems/PopulationSimulation/WorkingSystem.cs
+++ Accropoly/Assets/0_Scripts/ECS/Systems/PopulationSimulation/WorkingSystem.cs
@@ -13,20 +13,28 @@ public partial class WorkingSystem : SystemBase
     }
     protected override void OnUpdate()
     {
+        Dependency.Complete();
+
         var ecb = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(World.Unmanaged);
+        int hours = SystemAPI.GetSingleton<GameInfo>().time.hours;
 
         Entities.WithDisabled<WantsToTravel, Travelling>().ForEach((Entity entity, ref Traveller traveller, in LocalTransform transform, in Person person, in Worker worker) =>
         {
             int2 pos = (int2)math.round(transform.Position.xz / 2);
-            if (pos.Equals(person.homeTile))
+            if (hours >= 16)
+            {
+                if (pos.Equals(person.homeTile)) return; // Skip people that are already at home
+
+                traveller.destination = person.homeTile;
+                ecb.SetComponentEnabled<WantsToTravel>(entity, true);
+
+            }
+            else if (hours >= 7)
             {
                 if (worker.employer.Equals(-1)) return; // Skip unemployed people
+                if (pos.Equals(worker.employer)) return; // Skip people that are already at work
+
                 traveller.destination = worker.employer;
-                ecb.SetComponentEnabled<WantsToTravel>(entity, true);
-            }
-            else
-            {
-                traveller.destination = person.homeTile;
                 ecb.SetComponentEnabled<WantsToTravel>(entity, true);
             }
         }).Schedule();
