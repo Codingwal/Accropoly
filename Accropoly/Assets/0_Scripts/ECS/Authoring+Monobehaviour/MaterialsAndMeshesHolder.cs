a21384ceef1afd1b442d11ba8de7c41cdcd2df82
Codingwal <florian.herzog08@gmail.com>
2025-06-16 16:00:16 +0200

    refactor: improve MaterialsAndMeshesHolder
    Use "DOTS systems" for rendering -> performance boost

--- Accropoly/Assets/0_Scripts/ECS/Authoring+Monobehaviour/MaterialsAndMeshesHolder.cs
+++ Accropoly/Assets/0_Scripts/ECS/Authoring+Monobehaviour/MaterialsAndMeshesHolder.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Rendering;
 using UnityEngine;
@@ -11,53 +12,72 @@ public class MaterialsAndMeshesHolder : MonoBehaviour
     [SerializeField] private SerializableDictionary<TileType, MaterialMeshPair> simpleTiles = new();
     [SerializeField] private SerializableDictionary<TileType, MaterialMeshPairSet> connectingTiles = new();
 
-    private static List<KeyValuePair<Entity, MaterialMeshPair>> entitiesToUpdate = new();
+    private NativeHashMap<int, MaterialMeshInfo> simpleTilesUnmanaged; // <TileType, pair>
+    private NativeHashMap<int, MaterialMeshInfoSet> connectingTilesUnmanaged; // <TileType, set>
+
     private void Awake()
     {
         if (instance != null) Debug.LogError("Found multiple instances of MaterialsAndMeshesHolder");
         instance = this;
     }
-    private void Update()
+    private void Start()
     {
-        if (entitiesToUpdate.Count == 0) return;
-        EntityManager entityManager = ECSUtility.EntityManager;
-        foreach (var (entity, materialMeshPair) in entitiesToUpdate)
+        var graphicsSystem = ECSUtility.World.GetOrCreateSystemManaged<EntitiesGraphicsSystem>();
+
+        simpleTilesUnmanaged = new(4, Allocator.Persistent);
+        foreach ((TileType tileType, MaterialMeshPair pair) in simpleTiles)
         {
-            if (!entityManager.Exists(entity)) continue;
+            Debug.Assert(pair.material != null, $"Material for tileType {tileType} is null");
+            Debug.Assert(pair.mesh != null, $"Mesh for tileType {tileType} is null");
 
-            entityManager.SetSharedComponentManaged(entity, new RenderMeshArray(
-                new Material[] { materialMeshPair.material },
-                new Mesh[] { materialMeshPair.mesh }
-            ));
+            BatchMaterialID matID = graphicsSystem.RegisterMaterial(pair.material);
+            BatchMeshID meshID = graphicsSystem.RegisterMesh(pair.mesh);
 
-            // id of -1 means index 0 of RenderMeshArray
-            int id = -1;
-            entityManager.SetComponentData(entity, new MaterialMeshInfo(new BatchMaterialID() { value = (uint)id }, new BatchMeshID() { value = (uint)id }));
+            simpleTilesUnmanaged.Add((int)tileType, new(matID, meshID));
         }
-        entitiesToUpdate.Clear();
+
+        connectingTilesUnmanaged = new(1, Allocator.Persistent);
+        foreach ((TileType tileType, MaterialMeshPairSet managedSet) in connectingTiles)
+        {
+            MaterialMeshInfoSet set = new(6, Allocator.Persistent);
+            for (int i = 0; i < 6; i++)
+            {
+                MaterialMeshPair pair = managedSet.pairs[i];
+
+                Debug.Assert(pair.material != null, $"Material for tileType {tileType} is null");
+                Debug.Assert(pair.mesh != null, $"Mesh for tileType {tileType} is null");
+
+                BatchMaterialID matID = graphicsSystem.RegisterMaterial(pair.material);
+                BatchMeshID meshID = graphicsSystem.RegisterMesh(pair.mesh);
+
+                set.pairs[i] = new(matID, meshID);
+            }
+            connectingTilesUnmanaged.Add((int)tileType, set);
+        }
     }
-    public static MaterialMeshPair GetMaterialAndMesh(TileType tileType)
+    private void OnDestroy()
+    {
+        foreach (var pair in connectingTilesUnmanaged)
+        {
+            pair.Value.pairs.Dispose();
+        }
+
+        simpleTilesUnmanaged.Dispose();
+        connectingTilesUnmanaged.Dispose();
+    }
+    public static MaterialMeshInfo GetMaterialAndMesh(TileType tileType)
     {
         if (instance == null) Debug.LogError("Instance == null");
-        if (instance.simpleTiles.TryGetValue(tileType, out var pair))
-        {
-            Debug.Assert(pair.material != null, $"Material for tileType {tileType} is null");
-            Debug.Assert(pair.mesh != null, $"Mesh for tileType {tileType} is null");
+        if (instance.simpleTilesUnmanaged.TryGetValue((int)tileType, out var pair))
             return pair;
-        }
-        else if (instance.connectingTiles.TryGetValue(tileType, out var set))
-        {
-            pair = set.pairs[0];
-            Debug.Assert(pair.material != null, $"Material for tileType {tileType} is null");
-            Debug.Assert(pair.mesh != null, $"Mesh for tileType {tileType} is null");
-            return pair;
-        }
+        else if (instance.connectingTilesUnmanaged.TryGetValue((int)tileType, out var set))
+            return set.pairs[0];
         else Debug.LogError($"Material & Mesh for tileType {tileType} is missing");
         return default;
     }
-    public static void UpdateMeshAndMaterial(Entity entity, MaterialMeshPair materialMeshPair)
+    public static void UpdateMeshAndMaterial(Entity entity, MaterialMeshInfo materialMeshInfo)
     {
-        entitiesToUpdate.Add(new(entity, materialMeshPair));
+        ECSUtility.EntityManager.SetComponentData(entity, materialMeshInfo);
     }
     public static void UpdateMeshAndMaterial(Entity entity, TileType newTileType)
     {
@@ -65,20 +85,14 @@ public class MaterialsAndMeshesHolder : MonoBehaviour
     }
     public static void UpdateAppearence(Entity entity, TileType tileType, Components.ConnectingTile connectingTile)
     {
-        if (instance == null) Debug.LogError("Instance == null");
-
-        if (instance.connectingTiles.TryGetValue(tileType, out var set))
+        if (instance.connectingTilesUnmanaged.TryGetValue((int)tileType, out var set))
         {
             int index = connectingTile.GetIndex();
             var pair = set.pairs[index];
 
-            // Update visuals
-            Debug.Assert(pair.material != null, $"Material for tileType {tileType} is null");
-            Debug.Assert(pair.mesh != null, $"Mesh for tileType {tileType} is null");
             UpdateMeshAndMaterial(entity, pair);
         }
-        else if (instance.simpleTiles.Contains(tileType)) Debug.LogError($"TileType {tileType} is a simpleTile, not a connectingTile");
-        else Debug.LogError($"Material & Mesh for tileType {tileType} is missing");
+        else Debug.LogError($"Material & Mesh for tileType {tileType} is missing (Assumed to be a connectingTile)");
     }
 }
 [Serializable]
@@ -91,4 +105,13 @@ public struct MaterialMeshPair
 public class MaterialMeshPairSet
 {
     public MaterialMeshPair[] pairs = new MaterialMeshPair[6];
+}
+
+public struct MaterialMeshInfoSet
+{
+    public NativeArray<MaterialMeshInfo> pairs;
+    public MaterialMeshInfoSet(int length, Allocator allocator)
+    {
+        pairs = new(length, allocator);
+    }
 }
--- Accropoly/Assets/0_Scripts/Utility/Serializable/SerializableKeyValuePair.cs
+++ Accropoly/Assets/0_Scripts/Utility/Serializable/SerializableKeyValuePair.cs
@@ -5,4 +5,9 @@ public struct SerializableKeyValuePair<TKey, TValue>
 {
     public TKey key;
     public TValue value;
+    public void Deconstruct(out TKey key, out TValue value)
+    {
+        key = this.key;
+        value = this.value;
+    }
 }
