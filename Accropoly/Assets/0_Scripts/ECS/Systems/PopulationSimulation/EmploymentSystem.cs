17660eed5dde1f9ba8fea9cbd879350bfa0c00d2
Codingwal <florian.herzog08@gmail.com>
2025-06-16 11:57:34 +0200

    refactor: improve EmploymentSystem
    Simpler but better code :D

--- Accropoly/Assets/0_Scripts/ECS/Systems/PopulationSimulation/EmploymentSystem.cs
+++ Accropoly/Assets/0_Scripts/ECS/Systems/PopulationSimulation/EmploymentSystem.cs
@@ -4,6 +4,7 @@ using Unity.Jobs;
 using Components;
 using Tags;
 using Unity.Mathematics;
+using UnityEngine;
 
 namespace Systems
 {
@@ -25,35 +26,43 @@ namespace Systems
 
             var entityGrid = TileGridUtility.GetEntityGrid();
 
-            NativeArray<Entity> employerEntities = employersWithSpaceQuery.ToEntityArray(Allocator.TempJob);
-            NativeArray<Employer> employers = employersWithSpaceQuery.ToComponentDataArray<Employer>(Allocator.TempJob);
-            NativeArray<Tile> employerMapTileComponents = employersWithSpaceQuery.ToComponentDataArray<Tile>(Allocator.TempJob);
-            NativeArray<int> index = new(1, Allocator.TempJob);
+            NativeList<Entity> employerEntities = employersWithSpaceQuery.ToEntityListAsync(Allocator.TempJob, out var handle);
+            handle.Complete();
 
             // Employ people
             Entities.WithAll<Unemployed>().ForEach((Entity entity, ref Worker worker, in Person person) =>
             {
-                if (index[0] >= employers.Length) return;
-
-                Employer employer = employers[index[0]];
-                int2 employerPos = employerMapTileComponents[index[0]].pos;
-
-                if (PathfindingSystem.CalculateTravelTime(person.homeTile, employerPos, entityGrid) == -1) // No valid path to the employer
+                if (employerEntities.IsEmpty)
                     return;
 
-                employer.freeSpace--;
-                employers[index[0]] = employer; // Save the updated employer value
-                ecb.SetComponent(employerEntities[index[0]], employer); // Save the updated employer value
-                worker.employer = employerPos; // Store the employer position
-
-                ecb.RemoveComponent<Unemployed>(entity); // The worker is no longer searching
-
-                if (employer.freeSpace == 0)
+                for (int i = 0; i < employerEntities.Length; i++)
                 {
-                    ecb.RemoveComponent<HasSpace>(employerEntities[index[0]]);
-                    index[0]++; // Move to next employer
+                    Entity employerEntity = employerEntities[i];
+                    Employer employer = SystemAPI.GetComponent<Employer>(employerEntity);
+                    int2 employerPos = SystemAPI.GetComponent<Tile>(employerEntity).pos;
+
+                    if (employer.freeSpace == 0) // HasSpace tag might still be present because the employer was filled in this frame
+                        Debug.LogError("!");
+
+                    if (PathfindingSystem.CalculateTravelTime(person.homeTile, employerPos, entityGrid) == -1) // No valid path to the employer
+                        continue;
+
+                    // Update employer
+                    employer.freeSpace--;
+                    SystemAPI.SetComponent(employerEntity, employer); // Can't use ecb because the field might be updated multiple times
+                    if (employer.freeSpace == 0)
+                    {
+                        ecb.RemoveComponent<HasSpace>(employerEntity);
+                        employerEntities.RemoveAtSwapBack(i); // Following unemployed people should not check full employers
+                    }
+
+                    // Update person
+                    worker.employer = employerPos;
+                    ecb.RemoveComponent<Unemployed>(entity);
+
+                    break; // Stop searching for an employer if a valid employer has been found
                 }
-            }).WithDisposeOnCompletion(employerEntities).WithDisposeOnCompletion(employers).WithDisposeOnCompletion(employerMapTileComponents).WithDisposeOnCompletion(index)
+            }).WithDisposeOnCompletion(employerEntities)
             .Run(); // Must be executed on main thread because PathfindingSystem.CalculateTravelTime() is called (Unity throws errors if Schedule() is used)
 
             // Remove people from their workplace if there is no valid path
@@ -75,7 +84,7 @@ namespace Systems
                 // Make person unemployed
                 ecb.AddComponent<Unemployed>(entity);
                 ecb.SetComponent(entity, new Worker { employer = -1, timeToWork = -1 });
-            }).Run();
+            }).Run(); // Must be executed on main thread because PathfindingSystem.CalculateTravelTime() is called (Unity throws errors if Schedule() is used)
         }
     }
 }
