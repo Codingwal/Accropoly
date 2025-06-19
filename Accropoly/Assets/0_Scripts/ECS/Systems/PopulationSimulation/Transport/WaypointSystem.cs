0c190c288d42280bf558446e03ef8856c982d10d
Codingwal <florian.herzog08@gmail.com>
2025-06-19 09:57:37 +0200

    setup

---
+++ Accropoly/Assets/0_Scripts/ECS/Systems/PopulationSimulation/Transport/WaypointSystem.cs
@@ -0,0 +1,13 @@
+using Unity.Entities;
+
+public partial class WaypointSystem : SystemBase
+{
+    protected override void OnCreate()
+    {
+
+    }
+    protected override void OnUpdate()
+    {
+
+    }
+}
